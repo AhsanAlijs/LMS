bf42c59de88c1d61773a80bfa21dd1f3a2bb6a44
ahsanalijs <ahsanali45444@gmail.com>
2024-02-16 02:07:52 +0500

    Add file and pakeges

--- src/screens/AddmissionPage/AddmissionForm.jsx
+++ src/screens/AddmissionPage/AddmissionForm.jsx
@@ -1,16 +1,47 @@
-import { Box, TextField, Typography } from "@mui/material";
-import React from "react";
+import {
+  Box,
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { styled } from "@mui/material/styles";
+import React, { useState } from "react";
+import { DatePicker } from "@mui/x-date-pickers";
+import Button from "@mui/material/Button";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
 const AddmissionForm = () => {
+  const [age, setAge] = useState("");
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+  // const VisuallyHiddenInput = styled("input")({
+  //   clip: "rect(0 0 0 0)",
+  //   clipPath: "inset(50%)",
+  //   height: 1,
+  //   overflow: "hidden",
+  //   position: "absolute",
+  //   bottom: 0,
+  //   left: 0,
+  //   whiteSpace: "nowrap",
+  // //   width: 1,
+  // });
   return (
     <>
       <Box
         sx={{
           backgroundImage:
-            'url("https://as2.ftcdn.net/v2/jpg/01/90/51/59/1000_F_190515991_puEUJm1SbgEmarc5nciJV48s20Yx3dxj.jpg")',
+            'url("https://files.123freevectors.com/wp-content/original/108145-light-orange-wave.jpg")',
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
-          height: "100vmin",
+          height: "100vh",
         }}
       >
         <div style={{ textAlign: "center", paddingTop: "40px" }}>
@@ -18,24 +49,58 @@ const AddmissionForm = () => {
             Admission Form Of <span style={{ color: "#251855" }}>(LMS)</span>
           </Typography>
         </div>
-        <Box sx={{ marginTop: "50px" }}>
-          <Typography>
+        <Box sx={{ marginTop: "50px", display:'flex',justifyContent:'center'  }}>
+          <Typography sx={{width: "90%"}}>
             <form>
-              <TextField
-                id="outlined-basic"
-                label="Outlined"
-                variant="outlined"
-                type="text"
-              />
-              <TextField
-                id="outlined-basic"
-                label="Outlined"
-                variant="outlined"
-                type="text"
-              />
-              <select>
-                <option value="//////">---------</option>
-              </select>
+              <Box className="d-flex justify-content-center align-item-center gap-5 flex-wrap">
+                <TextField
+                  id="outlined-basic"
+                  label="First Name"
+                  variant="outlined"
+                  type="text"
+                  className="input"
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="Last Name"
+                  variant="outlined"
+                  type="text"
+                  className="input"
+                />
+              </Box>
+              <Box className="d-flex align-item-center justify-content-center mt-3 ">
+                <FormControl className="selectInput">
+                  <InputLabel id="demo-simple-select-helper-label">
+                    Chos
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-helper-label"
+                    id="demo-simple-select-helper"
+                    value={age}
+                    label="Age"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Web Development</MenuItem>
+                    <MenuItem value={20}>Graphics Designing</MenuItem>
+                    <MenuItem value={30}>Ai Chat Boat</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+              <Box className="d-flex flex-wrap justify-content-center align-item-center mt-3">
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker className="dateInput" />
+                </LocalizationProvider>
+              </Box>
+              <Box className="d-flex flex-wrap justify-content-center align-item-center mt-2">
+                <TextField
+                  type="file"
+                  placeholder="jhjhjh"
+                  className="btn fileInput"
+                />
+              </Box>
             </form>
           </Typography>
         </Box>
